4257bb07a7cb431b3d4549f65b4bc79e7d58dd4c
Manav Misra <manavm@visionwebsoft.com>
2023-01-03 20:24:30 -0600

    Add the host to the config

--- app/config.js
+++ app/config.js
@@ -4,8 +4,9 @@ dotenv.config();
 
 export default {
   db: {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
     dbName: process.env.DB_NAME,
+    host: process.env.DB_HOST || "localhost",
+    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,
   },
 };
