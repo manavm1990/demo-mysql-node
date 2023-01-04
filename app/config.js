f7667e3b390f3a1df06d902a91395e435bd1ca27
Manav Misra <manavm@visionwebsoft.com>
2023-01-03 20:43:36 -0600

    fix: :wrench: specify 'database'

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   db: {
-    dbName: process.env.DB_NAME,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST || "localhost",
     password: process.env.DB_PASSWORD,
     user: process.env.DB_USER,
