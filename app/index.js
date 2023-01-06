be6bb1a5079485f987a99a24f82c282fe83f66b3
Manav Misra <manavm@visionwebsoft.com>
2023-01-05 20:08:16 -0600

    Show example of PREPARED STATEMENT with delete

--- app/index.js
+++ app/index.js
@@ -1,10 +1,9 @@
 import config from "./config.js";
 import mysql from "mysql2/promise";
-import { selectStar } from "./utils.js";
+import { delById } from "./utils.js";
 
 const conn = await mysql.createConnection(config.db);
 
-// We destructure the first element of the array returned by execute
-const [employees] = await selectStar(conn, "employee");
-console.log(employees);
+await delById({ connection: conn, tableName: "course_names", id: 3 });
+
 conn.end();
--- app/utils.js
+++ app/utils.js
@@ -1,3 +1,7 @@
 // This is a utility function that we can use with a PREPARED STATEMENT to...
 export const selectStar = (connection, tableName) =>
   connection.execute(`SELECT * FROM ${tableName}`);
+
+export const delById = ({ connection, tableName, id }) => {
+  connection.execute(`DELETE FROM ${tableName} WHERE id = ?`, [id]);
+};
