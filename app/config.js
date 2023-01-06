4559d8ddd8aea150b2d3251d2e3f02c5b6ff7308
Manav Misra <manavm@visionwebsoft.com>
2023-01-05 19:16:43 -0600

    docs: :bulb:

--- app/config.js
+++ app/config.js
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// We export an object with the necessary configuration
+// Other modules can then use this object as needed
+// Only this module directly accesses the environment variables
 export default {
   db: {
     database: process.env.DB_NAME,
